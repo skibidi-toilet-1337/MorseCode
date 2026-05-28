17106413f8374f1856e5b7caed96547380217553
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-05-28 17:32:21 +0700

    Добавлено два новых словаря, один для кодирования, другой для декодирования.

--- MorseCode/MorseAlphabet.cs
+++ MorseCode/MorseAlphabet.cs
@@ -5,7 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MorseCode {
-  static class MorseAlphabet {
-    static Dictionary<char, string> charToMorseCode;
+  internal static class MorseAlphabet {
+    private static readonly Dictionary<char, string> _charToMorse = new Dictionary<char, string> {
+        { 'A', ".-"},   {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},  {'E', "."},
+        {'F', "..-."}, {'G', "--."},  {'H', "...."}, {'I', ".."},   {'J', ".---"},
+        {'K', "-.-"},  {'L', ".-.."}, {'M', "--"},   {'N', "-."},   {'O', "---"},
+        {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."},  {'S', "..."},  {'T', "-"},
+        {'U', "..-"},  {'V', "...-"}, {'W', ".--"},  {'X', "-..-"}, {'Y', "-.--"},
+        {'Z', "--.."},
+        {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
+        {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"}
+    };
+
+    private static readonly Dictionary<string, char> _charFromMorse = _charToMorse.ToDictionary(pair => pair.Value, pair => pair.Key);
   }
 }
