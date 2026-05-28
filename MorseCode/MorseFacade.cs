033331f979364b867e191e4e5c7b9653e8be4408
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-05-28 21:00:36 +0700

    Добавлен класс MorseAlphabet с поддержкой латиницы и цифр. Создан модуль MorseTranslator для парсинга строк и логики разделения букв и слов. Реализован класс Beeper и паттерн Strategy. Добавлен паттерн Facade в MorseFacade для удобного управления подсистемами из главного цикла программы.

---
+++ MorseCode/MorseFacade.cs
@@ -0,0 +1,19 @@
+﻿public class MorseFacade {
+  private readonly MorseTranslator _translator;
+  private readonly IMorsePlayer _player;
+
+  public MorseFacade(MorseTranslator translator, IMorsePlayer player) {
+    _translator = translator;
+    _player = player;
+  }
+
+  public string EncodeAndPlay(string text) {
+    string morse = _translator.EncodeInput(text);
+    _player.Play(morse);
+    return morse;
+  }
+
+  public string Decode(string morseCode) {
+    return _translator.DecodeInput(morseCode);
+  }
+}
--- MorseCode/Program.cs
+++ MorseCode/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MorseCode {
-  internal class Program {
-    static void Main(string[] args) {
+internal class Program {
+  private static void Main() {
+    MorseTranslator translator = new MorseTranslator();
+    IMorsePlayer player = new Beeper();
+
+    MorseFacade morseManager = new MorseFacade(translator, player);
+
+    Console.WriteLine("Меню");
+
+    while (true) {
+      Console.WriteLine("Выберите действие:\n" +
+        "1.Закодировать текст и проиграть звук\n" +
+        "2.Декодировать код Морзе\n" +
+        "3.Выход\n");
+      Console.Write("Выбор: ");
+
+      string choice = Console.ReadLine();
+
+      if (choice == "3") {
+        break;
+      }
+
+      switch (choice) {
+        case "1":
+          Console.Write("Введите текст для кодирования: ");
+          string textInput = Console.ReadLine();
+          string encoded = translator.EncodeInput(textInput);
+          Console.WriteLine($"Результат в Морзе: {encoded}\n" +
+            "Воспроизведение сообщения в Морзе\n");
+          player.Play(encoded);
+          break;
+
+        case "2":
+          Console.Write("Введите код Морзе (буквы через 1 пробел, слова через 3 пробела): ");
+          string morseInput = Console.ReadLine();
+
+          string decoded = morseManager.Decode(morseInput);
+          Console.WriteLine($"Расшифрованный текст: {decoded}");
+          break;
+
+        default:
+          Console.WriteLine("Ошибка, введите свой выбор ещё раз");
+          break;
+      }
     }
   }
 }
